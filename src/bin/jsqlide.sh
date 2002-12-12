47e2778370b82ee8cb73207e425ac4b30cf72f26
davidmartinez <davidmartinez@6c44c650-1e0f-0410-8560-dbd493bbbe5d>
2002-12-12 03:59:46 +0000

    JSQLIDE Shell file

--- src/bin/jsqlide.sh
+++ src/bin/jsqlide.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-java -cp ../lib/jsqlide.jar:../lib/xerces.jar:../lib/mysql_comp.jar:$CLASSPATH com.hackerdude.devtools.db.sqlide.sqlide
+export JSQLIDE_HOME=..
+java -cp $JSQLIDE_HOME/lib/jsqlide.jar:$JSQLIDE_HOME/lib/xerces.jar:$JSQLIDE_HOME/lib/mysql_comp.jar:$CLASSPATH com.hackerdude.apps.sqlide.SqlIdeApplication
 
