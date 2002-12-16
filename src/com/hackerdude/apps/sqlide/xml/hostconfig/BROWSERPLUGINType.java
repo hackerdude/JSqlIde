5a397fc7d73606895886b365276684684cb6b2a1
David Martinez <david@hackerdude.com>
2002-12-16 23:03:55 +0000

    Added browser plugin new classes

---
+++ src/com/hackerdude/apps/sqlide/xml/hostconfig/BROWSERPLUGINType.java
@@ -0,0 +1,131 @@
+/*
+ * This class was automatically generated with 
+ * <a href="http://castor.exolab.org">Castor 0.9.4</a>, using an
+ * XML Schema.
+ * $Id$
+ */
+
+package com.hackerdude.apps.sqlide.xml.hostconfig;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Serializable;
+import java.io.Writer;
+import org.exolab.castor.xml.*;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.ValidationException;
+import org.xml.sax.ContentHandler;
+
+/**
+ * 
+ * 
+ * @version $Revision$ $Date$
+**/
+public abstract class BROWSERPLUGINType implements java.io.Serializable {
+
+
+      //--------------------------/
+     //- Class/Member Variables -/
+    //--------------------------/
+
+    private NamedQueries _namedQueries;
+
+    private PluginProperties _pluginProperties;
+
+
+      //----------------/
+     //- Constructors -/
+    //----------------/
+
+    public BROWSERPLUGINType() {
+        super();
+    } //-- com.hackerdude.apps.sqlide.xml.hostconfig.BROWSERPLUGINType()
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+     * Returns the value of field 'namedQueries'.
+     * 
+     * @return the value of field 'namedQueries'.
+    **/
+    public NamedQueries getNamedQueries()
+    {
+        return this._namedQueries;
+    } //-- NamedQueries getNamedQueries() 
+
+    /**
+     * Returns the value of field 'pluginProperties'.
+     * 
+     * @return the value of field 'pluginProperties'.
+    **/
+    public PluginProperties getPluginProperties()
+    {
+        return this._pluginProperties;
+    } //-- PluginProperties getPluginProperties() 
+
+    /**
+    **/
+    public boolean isValid()
+    {
+        try {
+            validate();
+        }
+        catch (org.exolab.castor.xml.ValidationException vex) {
+            return false;
+        }
+        return true;
+    } //-- boolean isValid() 
+
+    /**
+     * 
+     * 
+     * @param out
+    **/
+    public abstract void marshal(java.io.Writer out)
+        throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException;
+
+    /**
+     * 
+     * 
+     * @param handler
+    **/
+    public abstract void marshal(org.xml.sax.ContentHandler handler)
+        throws java.io.IOException, org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException;
+
+    /**
+     * Sets the value of field 'namedQueries'.
+     * 
+     * @param namedQueries the value of field 'namedQueries'.
+    **/
+    public void setNamedQueries(NamedQueries namedQueries)
+    {
+        this._namedQueries = namedQueries;
+    } //-- void setNamedQueries(NamedQueries) 
+
+    /**
+     * Sets the value of field 'pluginProperties'.
+     * 
+     * @param pluginProperties the value of field 'pluginProperties'
+    **/
+    public void setPluginProperties(PluginProperties pluginProperties)
+    {
+        this._pluginProperties = pluginProperties;
+    } //-- void setPluginProperties(PluginProperties) 
+
+    /**
+    **/
+    public void validate()
+        throws org.exolab.castor.xml.ValidationException
+    {
+        org.exolab.castor.xml.Validator validator = new org.exolab.castor.xml.Validator();
+        validator.validate(this);
+    } //-- void validate() 
+
+}
---
+++ src/com/hackerdude/apps/sqlide/xml/hostconfig/BROWSERPLUGINTypeDescriptor.java
@@ -0,0 +1,197 @@
+/*
+ * This class was automatically generated with 
+ * <a href="http://castor.exolab.org">Castor 0.9.4</a>, using an
+ * XML Schema.
+ * $Id$
+ */
+
+package com.hackerdude.apps.sqlide.xml.hostconfig;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+import org.exolab.castor.mapping.AccessMode;
+import org.exolab.castor.mapping.ClassDescriptor;
+import org.exolab.castor.mapping.FieldDescriptor;
+import org.exolab.castor.xml.*;
+import org.exolab.castor.xml.FieldValidator;
+import org.exolab.castor.xml.TypeValidator;
+import org.exolab.castor.xml.XMLFieldDescriptor;
+import org.exolab.castor.xml.handlers.*;
+import org.exolab.castor.xml.util.XMLFieldDescriptorImpl;
+import org.exolab.castor.xml.validators.*;
+
+/**
+ * 
+ * 
+ * @version $Revision$ $Date$
+**/
+public class BROWSERPLUGINTypeDescriptor extends org.exolab.castor.xml.util.XMLClassDescriptorImpl {
+
+
+      //--------------------------/
+     //- Class/Member Variables -/
+    //--------------------------/
+
+    private java.lang.String nsPrefix;
+
+    private java.lang.String nsURI;
+
+    private java.lang.String xmlName;
+
+    private org.exolab.castor.xml.XMLFieldDescriptor identity;
+
+
+      //----------------/
+     //- Constructors -/
+    //----------------/
+
+    public BROWSERPLUGINTypeDescriptor() {
+        super();
+        nsURI = "urn:hackerdude.com/Schemas/2002/databasespec_1.0.xsd";
+        xmlName = "BROWSERPLUGINType";
+        
+        //-- set grouping compositor
+        setCompositorAsSequence();
+        XMLFieldDescriptorImpl  desc           = null;
+        XMLFieldHandler         handler        = null;
+        FieldValidator          fieldValidator = null;
+        //-- initialize attribute descriptors
+        
+        //-- initialize element descriptors
+        
+        //-- _namedQueries
+        desc = new XMLFieldDescriptorImpl(NamedQueries.class, "_namedQueries", "namedQueries", NodeType.Element);
+        handler = (new XMLFieldHandler() {
+            public java.lang.Object getValue( java.lang.Object object ) 
+                throws IllegalStateException
+            {
+                BROWSERPLUGINType target = (BROWSERPLUGINType) object;
+                return target.getNamedQueries();
+            }
+            public void setValue( java.lang.Object object, java.lang.Object value) 
+                throws IllegalStateException, IllegalArgumentException
+            {
+                try {
+                    BROWSERPLUGINType target = (BROWSERPLUGINType) object;
+                    target.setNamedQueries( (NamedQueries) value);
+                }
+                catch (Exception ex) {
+                    throw new IllegalStateException(ex.toString());
+                }
+            }
+            public java.lang.Object newInstance( java.lang.Object parent ) {
+                return new NamedQueries();
+            }
+        } );
+        desc.setHandler(handler);
+        desc.setNameSpaceURI("urn:hackerdude.com/Schemas/2002/databasespec_1.0.xsd");
+        desc.setRequired(true);
+        desc.setMultivalued(false);
+        addFieldDescriptor(desc);
+        
+        //-- validation code for: _namedQueries
+        fieldValidator = new FieldValidator();
+        fieldValidator.setMinOccurs(1);
+        { //-- local scope
+        }
+        desc.setValidator(fieldValidator);
+        //-- _pluginProperties
+        desc = new XMLFieldDescriptorImpl(PluginProperties.class, "_pluginProperties", "pluginProperties", NodeType.Element);
+        handler = (new XMLFieldHandler() {
+            public java.lang.Object getValue( java.lang.Object object ) 
+                throws IllegalStateException
+            {
+                BROWSERPLUGINType target = (BROWSERPLUGINType) object;
+                return target.getPluginProperties();
+            }
+            public void setValue( java.lang.Object object, java.lang.Object value) 
+                throws IllegalStateException, IllegalArgumentException
+            {
+                try {
+                    BROWSERPLUGINType target = (BROWSERPLUGINType) object;
+                    target.setPluginProperties( (PluginProperties) value);
+                }
+                catch (Exception ex) {
+                    throw new IllegalStateException(ex.toString());
+                }
+            }
+            public java.lang.Object newInstance( java.lang.Object parent ) {
+                return new PluginProperties();
+            }
+        } );
+        desc.setHandler(handler);
+        desc.setNameSpaceURI("urn:hackerdude.com/Schemas/2002/databasespec_1.0.xsd");
+        desc.setMultivalued(false);
+        addFieldDescriptor(desc);
+        
+        //-- validation code for: _pluginProperties
+        fieldValidator = new FieldValidator();
+        { //-- local scope
+        }
+        desc.setValidator(fieldValidator);
+    } //-- com.hackerdude.apps.sqlide.xml.hostconfig.BROWSERPLUGINTypeDescriptor()
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+    **/
+    public org.exolab.castor.mapping.AccessMode getAccessMode()
+    {
+        return null;
+    } //-- org.exolab.castor.mapping.AccessMode getAccessMode() 
+
+    /**
+    **/
+    public org.exolab.castor.mapping.ClassDescriptor getExtends()
+    {
+        return null;
+    } //-- org.exolab.castor.mapping.ClassDescriptor getExtends() 
+
+    /**
+    **/
+    public org.exolab.castor.mapping.FieldDescriptor getIdentity()
+    {
+        return identity;
+    } //-- org.exolab.castor.mapping.FieldDescriptor getIdentity() 
+
+    /**
+    **/
+    public java.lang.Class getJavaClass()
+    {
+        return com.hackerdude.apps.sqlide.xml.hostconfig.BROWSERPLUGINType.class;
+    } //-- java.lang.Class getJavaClass() 
+
+    /**
+    **/
+    public java.lang.String getNameSpacePrefix()
+    {
+        return nsPrefix;
+    } //-- java.lang.String getNameSpacePrefix() 
+
+    /**
+    **/
+    public java.lang.String getNameSpaceURI()
+    {
+        return nsURI;
+    } //-- java.lang.String getNameSpaceURI() 
+
+    /**
+    **/
+    public org.exolab.castor.xml.TypeValidator getValidator()
+    {
+        return this;
+    } //-- org.exolab.castor.xml.TypeValidator getValidator() 
+
+    /**
+    **/
+    public java.lang.String getXMLName()
+    {
+        return xmlName;
+    } //-- java.lang.String getXMLName() 
+
+}
