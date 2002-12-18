ea2881049e99ad419bdeb5d3ea480d6d5b16b524
David Martinez <david@hackerdude.com>
2002-12-18 16:35:45 +0000

    Now fonts are by name so there can be a number of them.

--- src/com/hackerdude/apps/sqlide/dialogs/DlgPanelEditorPage.java
+++ src/com/hackerdude/apps/sqlide/dialogs/DlgPanelEditorPage.java
@@ -1,24 +1,24 @@
 /*
- * DlgPanelEditorPage.java - "Editor" Page of the
- * configuration dialog.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * Revision: $Revision$
- * Id      : $Id$
- *
+* DlgPanelEditorPage.java - "Editor" Page of the
+* configuration dialog.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+* Revision: $Revision$
+* Id      : $Id$
+*
 */
 
 package com.hackerdude.apps.sqlide.dialogs;
@@ -30,83 +30,100 @@ import javax.swing.*;
 
 import com.hackerdude.apps.sqlide.*;
 import com.hackerdude.swing.picklist.font.FontPickListDialog;
+import java.util.ArrayList;
 
 public class DlgPanelEditorPage extends JPanel {
 
-	final Action ACTION_CHANGE_SQLCODEFONT = new ActionChangeSQLFont();
-	final Action ACTION_CHANGE_RESULTSETFONT = new ActionChangeResultSetFont();
 
-    BorderLayout borderLayout1 = new BorderLayout();
-    JPanel jPanel1 = new JPanel();
-    GridBagLayout gridBagLayout1 = new GridBagLayout();
-    JPanel pnTopPane2;
-    FontDescriptionLabel lblResultSetFontDescription;
-    JPanel pnTopPane;
-    FontDescriptionLabel lblSqlFontDescription;
-    BorderLayout borderLayout2 = new BorderLayout();
-    BorderLayout borderLayout3 = new BorderLayout();
-    private JButton btnChangeSQLCodeFont = new JButton(ACTION_CHANGE_SQLCODEFONT);
-    private JButton btnChange = new JButton(ACTION_CHANGE_RESULTSETFONT);
-    private JLabel lblAppFonts = new JLabel();
+	BorderLayout borderLayout1 = new BorderLayout();
+	JPanel jPanel1 = new JPanel();
+	GridBagLayout gridBagLayout1 = new GridBagLayout();
+	private JLabel lblAppFonts = new JLabel();
 
-	private Font currentResultSetFont;
-	private Font currentSQLFont;
 
-    public DlgPanelEditorPage() {
+	ActionChangeSQLFont[] actions;
+    private JScrollPane jScrollPane1 = new JScrollPane();
 
-      setLayout( new BoxLayout(this, BoxLayout.Y_AXIS) );
-      jbInit();
+	public DlgPanelEditorPage() {
 
-   }
+		setLayout( new BoxLayout(this, BoxLayout.Y_AXIS) );
+		jbInit();
 
-   public void jbInit() {
-      pnTopPane2 = new JPanel();
-        lblResultSetFontDescription = new FontDescriptionLabel("ResultSet Font: ");
-        pnTopPane = new JPanel();
-        lblSqlFontDescription = new FontDescriptionLabel("SQL Code Font: ");
-        this.addFocusListener(new java.awt.event.FocusAdapter() {
-      });
-      this.setLayout(borderLayout1);
-        jPanel1.setLayout(gridBagLayout1);
-        pnTopPane2.setLayout(borderLayout2);
-        pnTopPane.setLayout(borderLayout3);
-        btnChangeSQLCodeFont.setText("Change...");
-        btnChange.setText("Change...");
-        lblResultSetFontDescription.setText("ResultSet: ");
-        lblSqlFontDescription.setText("SQL Code: ");
-        lblAppFonts.setHorizontalAlignment(SwingConstants.CENTER);
-        lblAppFonts.setHorizontalTextPosition(SwingConstants.LEFT);
-        lblAppFonts.setText("Application Fonts: ");
-        this.add(jPanel1,  BorderLayout.CENTER);
-        pnTopPane2.add(lblResultSetFontDescription,  BorderLayout.WEST);
-        jPanel1.add(pnTopPane,    new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0
+	}
+
+	public void jbInit() {
+		this.addFocusListener(new java.awt.event.FocusAdapter() {
+		});
+		this.setLayout(borderLayout1);
+		jPanel1.setLayout(gridBagLayout1);
+		lblAppFonts.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAppFonts.setHorizontalTextPosition(SwingConstants.LEFT);
+		lblAppFonts.setText("Application Fonts: ");
+		this.add(jPanel1,  BorderLayout.CENTER);
+        jPanel1.add(jScrollPane1,      new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0
             ,GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-        pnTopPane.add(lblSqlFontDescription, BorderLayout.CENTER);
-        jPanel1.add(pnTopPane2,   new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0
-            ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
-        pnTopPane.add(btnChangeSQLCodeFont,  BorderLayout.EAST);
-        pnTopPane2.add(btnChange, BorderLayout.EAST);
-        jPanel1.add(lblAppFonts,  new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
+        jPanel1.add(lblAppFonts, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
             ,GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
 
 
-    }
+	}
 
 
-    public void readFromModel() {
-		currentSQLFont = ProgramConfig.getInstance().getSQLFont();
-		currentResultSetFont = ProgramConfig.getInstance().getResultSetFont();
-		lblSqlFontDescription.setFontToDescribe(currentSQLFont);
-		lblResultSetFontDescription.setFontToDescribe(currentResultSetFont);
+	public void readFromModel() {
+		jScrollPane1.getViewport().removeAll();
 
+		JPanel fontContainer = new JPanel(new GridBagLayout());
+
+		JPanel[] panels = getFontPanels();
+		for ( int i=0; i<panels.length; i++ ) {
+			fontContainer.add(panels[i],new GridBagConstraints(0, i, 1,1, 1,1,GridBagConstraints.CENTER,GridBagConstraints.REMAINDER, new Insets(0,0,0,0), 0,0));
+		}
+		jScrollPane1.getViewport().add(fontContainer);
 	}
 
 
 	public void applyToModel() {
-		if ( currentSQLFont != null ) ProgramConfig.getInstance().setSQLFont(currentSQLFont);
-		if ( currentResultSetFont != null ) ProgramConfig.getInstance().setResultSetFont(currentResultSetFont);
+		for ( int i=0; i<ProgramConfig.FONT_NAMES.length; i++ ) {
+			Font newFont = actions[0].getFont();
+			ProgramConfig.getInstance().setFont(ProgramConfig.FONT_NAMES[i], newFont);
+		}
 	}
 
+	public ActionChangeSQLFont[] initializeFontActions() {
+		ArrayList arrayList = new ArrayList();
+		for ( int i=0; i<ProgramConfig.FONT_NAMES.length; i++ ) {
+			String currentFontName = ProgramConfig.FONT_NAMES[i];
+			String title = "Change Font for "+currentFontName;
+			String instructions = "Compose a new Font for "+currentFontName;
+			ActionChangeSQLFont changeAction = new ActionChangeSQLFont(SqlIdeApplication.getFrame(),
+			title, instructions,
+   ProgramConfig.getInstance().getFont(currentFontName), new FontDescriptionLabel(currentFontName+": "));
+			arrayList.add(changeAction);
+		}
+		ActionChangeSQLFont[] result = new ActionChangeSQLFont[arrayList.size()];
+		result = (ActionChangeSQLFont[])arrayList.toArray((ActionChangeSQLFont[])result);
+		return result;
+	}
+
+	public JPanel[] getFontPanels() {
+		actions = initializeFontActions();
+		JPanel[] result = new JPanel[actions.length];
+		for ( int i=0; i<actions.length; i++) {
+			FontDescriptionLabel label = actions[i].getDescriptionLabel();
+			JButton button = new JButton(actions[i]);
+			JPanel panel = new JPanel(new BorderLayout());
+			panel.add(label, BorderLayout.CENTER);
+			panel.add(button, BorderLayout.EAST);
+			result[i] = panel;
+		}
+		return result;
+
+	}
+
+	/**
+	 * This is a subclassing of JLabel to show the font description as well
+	 * as the specified text.
+	 */
 	class FontDescriptionLabel extends JLabel {
 
 		Font fontToDescribe;
@@ -124,7 +141,7 @@ public class DlgPanelEditorPage extends JPanel {
 
 		public String getText() {
 			String text = super.getText();
-			text = text+getFontDescription();
+			text = text+getFontDescription()+" ";
 			return text;
 		}
 
@@ -143,32 +160,41 @@ public class DlgPanelEditorPage extends JPanel {
 		}
 	}
 
+
 	class ActionChangeSQLFont extends AbstractAction {
-		public ActionChangeSQLFont() {
+
+		JFrame owner;
+		String title;
+		String instructions;
+		Font currentFont;
+		FontDescriptionLabel fontLabel;
+
+		public ActionChangeSQLFont(JFrame owner, String title, String instructions, Font currentFont, FontDescriptionLabel descriptionLabel) {
+			super("..");
+			this.owner = owner;
+			this.title = title;
+			this.instructions = instructions;
+			this.currentFont = currentFont;
+			this.fontLabel = descriptionLabel;
+			fontLabel.setFontToDescribe(currentFont);
 		}
 
 		public void actionPerformed(ActionEvent evt) {
-			Font newFont = FontPickListDialog.showFontSelectionDialog(SqlIdeApplication.getFrame(), "Change SQL Editor Font", "Compose a new Font for the SQL Editor", currentSQLFont);
+//			Font newFont = FontPickListDialog.showFontSelectionDialog(SqlIdeApplication.getFrame(), "Change SQL Editor Font", "Compose a new Font for the SQL Editor", currentSQLFont);
+			Font newFont = FontPickListDialog.showFontSelectionDialog(owner, title, instructions, currentFont);
 			if ( newFont != null ) {
-				currentSQLFont = newFont;
-				lblSqlFontDescription.setFontToDescribe(newFont);
+				currentFont = newFont;
+				fontLabel.setFontToDescribe(newFont);
 			}
+		}
+
+		public Font getFont() {
+			return currentFont;
+		}
+		public FontDescriptionLabel getDescriptionLabel() {
+			return fontLabel;
 		}
 	}
 
-
-	class ActionChangeResultSetFont extends AbstractAction {
-		public ActionChangeResultSetFont() {
-		}
-
-		public void actionPerformed(ActionEvent evt) {
-			Font newFont = FontPickListDialog.showFontSelectionDialog(SqlIdeApplication.getFrame(), "Change ResultSet Font", "Compose a new font for the ResultSet", currentResultSetFont);
-			if ( newFont != null ) {
-				currentResultSetFont = newFont;
-				lblResultSetFontDescription.setFontToDescribe(newFont);
-			}
-		}
-	}
 
 }
-
